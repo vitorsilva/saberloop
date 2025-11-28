8c3a78fa489820e334392af2272bc410ecfca9cb
Vitor Silva <vitorsilva.com@gmail.com>
2025-11-28 17:56:35 +0000

    chore: version update

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-  export const APP_VERSION = '20251128.10';
-  export const BUILD_DATE = '2025-11-28T17:38:24.955Z';
+  export const APP_VERSION = '20251128.11';
+  export const BUILD_DATE = '2025-11-28T17:54:04.981Z';
   
