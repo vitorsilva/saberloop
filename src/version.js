f8ecb6c92012197ed9704c8fa25075619750181d
Vitor Silva <vitorsilva.com@gmail.com>
2025-12-12 18:22:06 +0000

    chore: rename repository from demo-pwa-app to saberloop

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-  export const APP_VERSION = '20251212.41';
-  export const BUILD_DATE = '2025-12-12T17:41:53.430Z';
+  export const APP_VERSION = '20251212.42';
+  export const BUILD_DATE = '2025-12-12T18:21:52.724Z';
   
