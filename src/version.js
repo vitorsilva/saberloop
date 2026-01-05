d17804bb4d55a1754df8e7de6f18c6d7c18d452a
Vitor Silva <vitorsilva.com@gmail.com>
2026-01-05 21:54:10 +0000

    chore: update version number, remove settings.local

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-  export const APP_VERSION = '20260104.221';
-  export const BUILD_DATE = '2026-01-04T15:03:09.879Z';
+  export const APP_VERSION = '20260105.224';
+  export const BUILD_DATE = '2026-01-05T21:34:29.800Z';
   
