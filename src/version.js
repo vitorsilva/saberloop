6800e27cba185f5a8ab781a33dcd7a4f7c40b5d6
Vitor Silva <vitorsilva.com@gmail.com>
2026-01-07 10:31:28 +0000

    feat(sharing): Add i18n strings for import UI in all 9 languages

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // Auto-generated - do not edit manually
-  export const APP_VERSION = '20260106.227';
-  export const BUILD_DATE = '2026-01-06T18:30:18.959Z';
+  export const APP_VERSION = '20260107.228';
+  export const BUILD_DATE = '2026-01-07T10:30:45.556Z';
   
